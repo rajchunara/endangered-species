515049a89b0c80966ed1f4491c7ea7299036cdb2
rajchunara <rajchunara777@gmail.com>
2020-12-30 19:06:55 -0500

    added social media icons in footer and made it responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import FacebookLogo from '../../logos/facebook.svg';
+import InstagramLogo from '../../logos/instagram-logo.svg';
+import TelegramLogo from '../../logos/telegram-logo.svg';
 import './footer.css';
 
 export default function Footer() {
+  let logoHeight = 30;
+  let logoWidth = 30;
+
   return (
     <div className="footer">
       <div className="footer-info-container">
@@ -9,6 +15,28 @@ export default function Footer() {
           Information credits: animalplanet.com
         </div>
         <div className="image-credits">Images credits: unsplash</div>
+      </div>
+
+      <div className="footer-icons">
+        <img
+          className="logo"
+          src={FacebookLogo}
+          height={logoHeight}
+          width={logoWidth}
+          alt="facebook logo"
+        />
+        <img
+          src={InstagramLogo}
+          height={logoHeight}
+          width={logoWidth}
+          alt="instagram logo"
+        />
+        <img
+          src={TelegramLogo}
+          height={logoHeight}
+          width={logoWidth}
+          alt="telegram logo"
+        />
       </div>
     </div>
   );
