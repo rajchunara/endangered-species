9c3d88b87079f731adc640385e05a406528e13f2
rajchunara <rajchunara777@gmail.com>
2020-12-31 00:00:53 -0500

    added hover effect and added twitter logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import FacebookLogo from '../../logos/facebook.svg';
-import InstagramLogo from '../../logos/instagram-logo.svg';
-import TelegramLogo from '../../logos/telegram-logo.svg';
+import React, { useState } from 'react';
+import { ReactComponent as FacebookLogo } from '../../logos/facebook.svg';
+import { ReactComponent as InstagramLogo } from '../../logos/instagram-logo.svg';
+import { ReactComponent as TelegramLogo } from '../../logos/telegram-logo.svg';
+import { ReactComponent as TwitterLogo } from '../../logos/Twitter.svg';
 import './footer.css';
 
 export default function Footer() {
   let logoHeight = 30;
   let logoWidth = 30;
+
+  const [isFacebookHovered, setIsFacebookHovered] = useState(false);
+  const [isInstagramHovered, setIsInstagramHovered] = useState(false);
+  const [isTwitterHovered, setIsTwitterHovered] = useState(false);
+  const [isTelegramHovered, setIsTelegramHovered] = useState(false);
 
   return (
     <div className="footer">
@@ -18,24 +24,45 @@ export default function Footer() {
       </div>
 
       <div className="footer-icons">
-        <img
+        {/* <img
           className="logo"
           src={FacebookLogo}
           height={logoHeight}
           width={logoWidth}
           alt="facebook logo"
-        />
-        <img
-          src={InstagramLogo}
-          height={logoHeight}
+        /> */}
+
+        <FacebookLogo
+          fill={isFacebookHovered ? '#1873EB' : '#FFFFFF'}
+          onMouseEnter={() => setIsFacebookHovered(true)}
+          onMouseLeave={() => setIsFacebookHovered(false)}
+          className="logo"
           width={logoWidth}
-          alt="instagram logo"
-        />
-        <img
-          src={TelegramLogo}
           height={logoHeight}
+        />
+        <InstagramLogo
+          fill={isInstagramHovered ? '#C53594' : '#ffffff'}
+          onMouseEnter={() => setIsInstagramHovered(true)}
+          onMouseLeave={() => setIsInstagramHovered(false)}
+          className="logo"
           width={logoWidth}
-          alt="telegram logo"
+          height={logoHeight}
+        />
+        <TwitterLogo
+          fill={isTwitterHovered ? '#55ACEE' : '#ffffff'}
+          onMouseEnter={() => setIsTwitterHovered(true)}
+          onMouseLeave={() => setIsTwitterHovered(false)}
+          className="logo"
+          width={logoWidth}
+          height={logoHeight}
+        />
+        <TelegramLogo
+          fill={isTelegramHovered ? '#0088CC' : '#ffffff'}
+          onMouseEnter={() => setIsTelegramHovered(true)}
+          onMouseLeave={() => setIsTelegramHovered(false)}
+          className="logo"
+          width={logoWidth}
+          height={logoHeight}
         />
       </div>
     </div>
